d816a3bc4c84439ac87fadffa5ee01961409006b
justindao549 <justindao549@gmail.com>
2022-11-28 11:51:00 -0800

    Week 4 Workshop

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,45 +1,63 @@
 const express = require('express');
+const authenticate = require('../authenticate');
 const Partner = require('../models/partner');
 const partnerRouter = express.Router();
+const cors = require('./cors');
 
 partnerRouter
 	.route('/')
-	.get((req, res, next) => {
-		Partner.find()
-			.then((partners) => {
+	.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+	.get(cors.cors, (req, res, next) => {
+		Promotion.find()
+			.then((promotion) => {
 				res.statusCode = 200;
 				res.setHeader('Content-Type', 'application/json');
-				res.json(partners);
+				res.json(promotion);
 			})
 			.catch((err) => next(err));
-	})
-	.post(auth.verifyUser, auth.verifyPartner, (req, res, next) => {
-		Partner.create(req.body)
+		Partner.find()
 			.then((partner) => {
-				console.log('partner Created', partner);
 				res.statusCode = 200;
 				res.setHeader('Content-Type', 'application/json');
 				res.json(partner);
 			})
 			.catch((err) => next(err));
 	})
-	.put((req, res, next) => {
-		res.statusCode = 403;
-		res.end('PUT operation not supported on /partners');
-	})
-	.delete(auth.verifyUser, auth.verifyPartner, (req, res, next) => {
-		Partner.deleteMany()
-			.then((response) => {
-				res.statusCode = 200;
-				res.setHeader('Content-Type', 'application/json');
-				res.json(response);
-			})
-			.catch((err) => next(err));
-	});
+	.post(
+		cors.corsWithOptions,
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			Partner.create(req.body)
+				.then((partner) => {
+					console.log('partner Created', partner);
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'application/json');
+					res.json(partner);
+				})
+				.catch((err) => next(err));
+		}
+	)
+	.put(
+		cors.corsWithOptions,
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res) => {
+			res.statusCode = 403;
+			res.end('PUT operation not supported on /partners');
+		}
+	)
+	.delete(
+		cors.corsWithOptions,
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {}
+	);
 
 partnerRouter
 	.route('/:partnerId')
-	.get((req, res, next) => {
+	.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+	.get(cors.cors, (req, res, next) => {
 		Partner.findById(req.params.partnerId)
 			.then((partner) => {
 				if (partner) {
@@ -54,50 +72,51 @@ partnerRouter
 			})
 			.catch((err) => next(err));
 	})
-	.post((req, res, next) => {
-		Partner.findById(req.params.partnerId)
-			.then((partner) => {
-				if (partner) {
-					partner.comments.push(req.body);
-					partner
-						.save()
-						.then((partner) => {
-							res.statusCode = 200;
-							res.setHeader('Content-Type', 'application/json');
-							res.json(partner);
-						})
-						.catch((err) => next(err));
-				} else {
-					err = new Error(`Partner ${req.params.partnerId} not found`);
-					err.status = 404;
-					return next(err);
-				}
-			})
-			.catch((err) => next(err));
-	})
-	.put(auth.verifyUser, auth.verifyPartner, (req, res, next) => {
-		Partner.findByIdAndUpdate(
-			req.params.partnerId,
-			{
-				$set: req.body,
-			},
-			{ new: true }
-		)
-			.then((partner) => {
-				res.statusCode = 200;
-				res.setHeader('Content-Type', 'application/json');
-				res.json(partner);
-			})
-			.catch((err) => next(err));
-	})
-	.delete(auth.verifyUser, auth.verifyPartner, (req, res, next) => {
-		Partner.findByIdAndDelete(req.params.partnerId)
-			.then((response) => {
-				res.statusCode = 200;
-				res.setHeader('Content-Type', 'application/json');
-				res.json(response);
-			})
-			.catch((err) => next(err));
-	});
+	.post(
+		cors.corsWithOptions,
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			Partner.findById(req.params.partnerId)
+				.then((partner) => {
+					if (partner) {
+						partner.comments.push(req.body);
+						partner
+							.save()
+							.then((partner) => {
+								res.statusCode = 200;
+								res.setHeader('Content-Type', 'application/json');
+								res.json(partner);
+							})
+							.catch((err) => next(err));
+					} else {
+						err = new Error(`Partner ${req.params.partnerId} not found`);
+						err.status = 404;
+						return next(err);
+					}
+				})
+				.catch((err) => next(err));
+		}
+	)
+	.put(
+		cors.corsWithOptions,
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res) => {}
+	)
+	.delete(
+		cors.corsWithOptions,
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			Promotion.findByIdAndDelete(req.params.promotionId)
+				.then((response) => {
+					res.statusCode = 200;
+					res.setHeader('Content-Type', 'application/json');
+					res.json(response);
+				})
+				.catch((err) => next(err));
+		}
+	);
 
 module.exports = partnerRouter;
